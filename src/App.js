5fa54d5d74a446be42c9a93986442ef13caa4457
Blake Simpson <38295317+EXC3ll3NTrhyTHM@users.noreply.github.com>
2021-10-19 23:09:01 -0500

    Setting up Firebase and built out Sign up page component (#14)
    * Added firebase to app
    
    * Adding logs, will be  removed; updated gitignore
    
    * Added working sign up page
    
    * Added Commerce Bank logo to sign up page
    
    * Added new padding for button
    
    * Adding padding to top of password labels
    
    * Changed button name
    
    * Updated sign up page header and button text
    
    * Updated confirm password label text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Components/NavBar';
-
+//import NavBar from './Components/NavBar';
+import fire from './config/firebase';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import Signup from './Components/Signup';
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <NavBar />
-    );
+        <Container className="d-flex align-items-center 
+        justify-content-center"
+        style={{mindHeight: "100vh"}}>
+            <div className="w-100" style={{ maxWidth: '400px'}}>
+                <Router>
+                    <AuthProvider>
+                        <Switch>
+                            <Route path="/login" component={Login} />
+                            <Route path="/signup" component={Signup} />
+                            <Route exact path="/" component={Signup} />
+                        </Switch>
+                    </AuthProvider>
+                </Router>
+            </div>
+        </Container>
+
+    )
 }
 
 export default App;
---
+++ src/Components/Signup.js
@@ -0,0 +1,68 @@
+import React, { useRef, useState } from "react"
+import { Card, Form, Button, Alert, Image } from 'react-bootstrap'
+import { useAuth } from '../context/AuthContext'
+
+export default function Signup() {
+    const emailRef = useRef();
+    const passwordRef = useRef(); 
+    const passwordConfirmRef = useRef();
+    const { signup } = useAuth();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if (passwordRef.current.value !== 
+            passwordConfirmRef.current.value) {
+                return setError('Passwords do not match')
+            }
+
+            try {
+                setError('')
+                setLoading(true)
+                await signup(emailRef.current.value, passwordRef.current.value)
+            } catch (e) {
+                setError('Failed to create an account' + e)
+            }
+
+            setLoading(false)
+    }
+
+    return (
+        <>
+            <div class="small-padding">
+                <Image src="fulllogo.png" class="commerce-bank-logo-padding" fluid />
+            </div>
+            <Card>
+                <Card.Body>
+                    {/* <div>
+                        <img src="/fulllogo.png" alt="Commerce Bank" />
+                    </div> */}
+                    <h3 className="text-center mb-4">Create a new account</h3>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required />
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label class="top-padding">Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required />
+                        </Form.Group>
+                        <Form.Group id="password-confirm">
+                            <Form.Label class="top-padding">Confirm Password</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmRef} required />
+                        </Form.Group>
+                        <div class="small-padding w-100">
+                            <Button disabled={loading} className="w-100" type="submit">Create Account</Button>
+                        </div> 
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? <a href="./Login">Log in</a>
+            </div>
+        </>
+    )
+}
