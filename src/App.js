a6d584bca407453ce35ef5a1d591bc0147a6466f
Blake Simpson <shadowcloud131@gmail.com>
2021-10-27 14:14:13 -0500

    Updated imports

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import fire from './config/firebase';
-import Login from './Components/Login';
-import Home from './Components/Home';
-import Signup from './Components/Signup';
-import Dashboard from './Components/Dashboard'
+import Login from './components/Login';
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Transactions from './components/Transactions'
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -21,7 +21,7 @@ function App() {
                         <Switch>
                             <Route path="/login" component={Login} />
                             <Route path="/signup" component={Signup} />
-                            <Route exact path="/" component={Dashboard} />
+                            <Route exact path="/" component={Transactions} />
                         </Switch>
                     </AuthProvider>
                 </Router>
