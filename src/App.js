4b14220a2f05ba7046f70255bedca92592fbd07d
Blake Simpson <shadowcloud131@gmail.com>
2021-11-01 23:43:34 -0500

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
-import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Transactions from './components/Transactions'
 import TransactionForm from './components/TransactionForm'
-import { Container } from 'react-bootstrap';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PrivateRoute from "./components/PrivateRoute"
