986c6a3e08a7beb5d65f32cd3d3d123da8bfee61
Blake Simpson <shadowcloud131@gmail.com>
2021-11-01 23:25:45 -0500

    Fab button uses router now

--- src/components/FloatingButton.js
+++ src/components/FloatingButton.js
@@ -2,20 +2,22 @@ import React from 'react'
 import { View, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native'
 import AddIcon from "@mui/icons-material/Add"
 import Fab from '@mui/material/Fab'
+import { useHistory} from "react-router-dom";
 
 export default function FloatingButton() {
         
+    const history = useHistory();
+
     function handleClick() {
         history.push("/transactionForm")
     }
-
 
     return (
         <View style={[styles.container]}>
             <TouchableWithoutFeedback>
                 <Animated.View style={[styles.button, styles.menu]}>
                     {/* sx is the bootstrap equivalent to react-native style*/}
-                    <Fab href="./TransactionForm">
+                    <Fab onClick={handleClick}>
                         <AddIcon sx={[styles.icon]} />
                     </Fab>
                 </Animated.View>
@@ -23,10 +25,6 @@ export default function FloatingButton() {
         </View>
     )
 }
-
-
-
-
 
 const styles = StyleSheet.create({
     container: {
