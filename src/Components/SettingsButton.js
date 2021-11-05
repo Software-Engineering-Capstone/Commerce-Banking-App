fd6176d4320358c5084283fa098655682ee043d2
Blake Simpson <shadowcloud131@gmail.com>
2021-11-04 23:46:31 -0500

    Dynamic settings button size

--- src/Components/SettingsButton.js
+++ src/Components/SettingsButton.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
 import {Button} from "react-bootstrap"
-import { useHistory} from "react-router-dom";
+import { useHistory} from "react-router-dom"
 import { useAuth } from '../context/AuthContext'
+import { Dimensions } from "react-native"
 
 export default function SettingsButton() {
         const [, setError] = useState('');
         const [, setLoading] = useState(false);
         const { logout } = useAuth();
         const history = useHistory();
+        var width = Dimensions.get('window').width;
+        var buttonSize;
+
+        console.log(width);
+
+        // Button changes size depending on device
+        if (width < 751) {
+            buttonSize = "sm"
+        } else {
+            buttonSize = "lg"
+        }
 
         async function handleLogout() {
             setError('') 
@@ -27,7 +39,7 @@ export default function SettingsButton() {
 
         return (
             <span className="settings-button">
-                <Button variant="secondary" onClick={handleLogout}>
+                <Button variant="secondary" size={buttonSize} onClick={handleLogout}>
                     <img src="settingscog3.png" alt="settings" width="35" height="35" />
                 </Button>
             </span>
