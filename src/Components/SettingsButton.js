c33e1791db912dbf1e17a80b378634535e10a61c
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2021-11-28 12:56:20 -0600

    Commented out components/variables that weren't in use to attempt to clear build errors when hosting on netlify

--- src/Components/SettingsButton.js
+++ src/Components/SettingsButton.js
@@ -1,23 +1,24 @@
 import React, { useState } from 'react'
-import {Button} from "react-bootstrap"
+// import {Button} from "react-bootstrap"
 import { useHistory} from "react-router-dom"
 import { useAuth } from '../context/AuthContext'
-import { Dimensions } from "react-native"
+// import { Dimensions } from "react-native"
 
 export default function SettingsButton() {
         const [, setError] = useState('');
         const [, setLoading] = useState(false);
         const { logout } = useAuth();
         const history = useHistory();
-        var width = Dimensions.get('window').width;
-        var buttonSize;
+      //  var width = Dimensions.get('window').width;
+       //  var buttonSize;
 
-        // Button changes size depending on device
+      /*  // Button changes size depending on device
         if (width < 751) {
             buttonSize = "sm"
         } else {
             buttonSize = ""
         }
+        */
 
         async function handleLogout() {
             setError('') 
