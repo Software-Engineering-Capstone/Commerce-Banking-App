c9903c6817fc61ee655383c76c0cacc3f02ea779
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2021-12-04 11:28:39 -0600

    Made minor adjustments to Transaction Form - card styling, title centered

--- src/Components/TransactionForm.js
+++ src/Components/TransactionForm.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react"
 import { Form, Button, FloatingLabel, Col, Row, Container } from 'react-bootstrap'
 import { fireDb } from "../config/firebase";
 import { useHistory} from "react-router-dom";
-import SettingsButton from "./SettingsButton"
+//import SettingsButton from "./SettingsButton"
 import '../App.css';
 
 //
@@ -76,71 +76,73 @@ const TransactionForm = (props) => {
                 {
                     // The maxWidth property controls the overall width of the page elements
                 }
-                <div className="w-100" style={{ maxWidth: '600px'}}>
-                    <div className="small-padding w-100">
-                        <div className="page-header bottom-padding-t-form">
-                            <h2 className="inline-header">
-                                Transaction Form 
-                                <SettingsButton />
-                            </h2>
-                        </div>
-                        <Form autoComplete="off" onSubmit={handleFormSubmit}>
-                            <Row>
-                                <Form.Group as={Col} id="accountType">
-                                    <FloatingLabel controlId="floatingInputGrid" label="Account Type">
-                                        <Form.Control onChange={handleInputChange} name="Account-Type" value={values["Account-Type"]} type="text" 
-                                        placeholder="Account Type" ref={accountType} required />
-                                    </FloatingLabel>
-                                </Form.Group>
-                                <Form.Group as={Col} id="acctId">
-                                    <FloatingLabel controlId="floatingInputGrid" label="Account ID">
-                                        <Form.Control onChange={handleInputChange} name="Acct-id" value={values["Acct-id"]} type="text" 
-                                        placeholder="Account ID" ref={acctId} required />
-                                    </FloatingLabel>
-                                </Form.Group>
-                            </Row>
-                            <Row>
-                                <Form.Group as={Col} id="amount" className="top-padding">
-                                    <FloatingLabel controlId="floatingInputGrid" label="Amount">
-                                        <Form.Control onChange={handleInputChange} name="Amount" value={values.Amount} type="text" 
-                                        placeholder="Amount" ref={amount} required />
-                                    </FloatingLabel>
-                                </Form.Group>
-                                <Form.Group as={Col} id="balance" className="top-padding">
-                                    <FloatingLabel controlId="floatingInputGrid" label="Balance">
-                                        <Form.Control onChange={handleInputChange} name="Balance" value={values.Balance} type="text" 
-                                        placeholder="Amount" ref={balance} required />
-                                    </FloatingLabel>
-                                </Form.Group>
-                            </Row>
-                            <Row>
-                                <Form.Group as={Col} id="location" className="top-padding">
-                                    <FloatingLabel controlId="floatingInputGrid" label="Location">
-                                        <Form.Control onChange={handleInputChange} name="Location" value={values.Location} type="text" 
-                                        placeholder="Amount" ref={location} required />
-                                    </FloatingLabel>
-                                </Form.Group>
-                            </Row>
-                            <Row>
-                                <Form.Group as={Col} id="processingDate" className="top-padding">
-                                    <FloatingLabel controlId="floatingInputGrid" label="Processing Date">
-                                        <Form.Control onChange={handleInputChange} name="Processing-Date" value={values["Processing-Date"]} type="text" 
-                                        placeholder="Processing Date" ref={processingDate} required />
-                                    </FloatingLabel>
-                                </Form.Group>
-                                <Form.Group as={Col} id="transactionType" className="top-padding">
-                                    <FloatingLabel controlId="floatingInputGrid" label="Transaction Type">
-                                        <Form.Control onChange={handleInputChange} name="Transaction-type" value={values["Transaction-type"]} type="text" 
-                                        placeholder="Transaction Type" ref={transactionType} required />
-                                    </FloatingLabel>
-                                </Form.Group>
-                            </Row>
-                            <div className="top-padding w-100">
-                                <Button type="submit">Submit</Button>
-                            </div> 
-                        </Form>
+                <div className="card">
+                    <div className="w-100" style={{ maxWidth: '600px'}}>
+
+                            <div className="form-header">
+                                <h2 className="form-title">
+                                    Transaction Form 
+                                </h2>
+                            </div>
+                            <Form autoComplete="off" onSubmit={handleFormSubmit}>
+                                <Row>
+                                    <Form.Group as={Col} id="accountType">
+                                        <FloatingLabel controlId="floatingInputGrid" label="Account Type">
+                                            <Form.Control onChange={handleInputChange} name="Account-Type" value={values["Account-Type"]} type="text" 
+                                            placeholder="Account Type" ref={accountType} required />
+                                        </FloatingLabel>
+                                    </Form.Group>
+                                    <Form.Group as={Col} id="acctId">
+                                        <FloatingLabel controlId="floatingInputGrid" label="Account ID">
+                                            <Form.Control onChange={handleInputChange} name="Acct-id" value={values["Acct-id"]} type="text" 
+                                            placeholder="Account ID" ref={acctId} required />
+                                        </FloatingLabel>
+                                    </Form.Group>
+                                </Row>
+                                <Row>
+                                    <Form.Group as={Col} id="amount" className="top-padding">
+                                        <FloatingLabel controlId="floatingInputGrid" label="Amount">
+                                            <Form.Control onChange={handleInputChange} name="Amount" value={values.Amount} type="text" 
+                                            placeholder="Amount" ref={amount} required />
+                                        </FloatingLabel>
+                                    </Form.Group>
+                                    <Form.Group as={Col} id="balance" className="top-padding">
+                                        <FloatingLabel controlId="floatingInputGrid" label="Balance">
+                                            <Form.Control onChange={handleInputChange} name="Balance" value={values.Balance} type="text" 
+                                            placeholder="Amount" ref={balance} required />
+                                        </FloatingLabel>
+                                    </Form.Group>
+                                </Row>
+                                <Row>
+                                    <Form.Group as={Col} id="location" className="top-padding">
+                                        <FloatingLabel controlId="floatingInputGrid" label="Location">
+                                            <Form.Control onChange={handleInputChange} name="Location" value={values.Location} type="text" 
+                                            placeholder="Amount" ref={location} required />
+                                        </FloatingLabel>
+                                    </Form.Group>
+                                </Row>
+                                <Row>
+                                    <Form.Group as={Col} id="processingDate" className="top-padding">
+                                        <FloatingLabel controlId="floatingInputGrid" label="Processing Date">
+                                            <Form.Control onChange={handleInputChange} name="Processing-Date" value={values["Processing-Date"]} type="text" 
+                                            placeholder="Processing Date" ref={processingDate} required />
+                                        </FloatingLabel>
+                                    </Form.Group>
+                                    <Form.Group as={Col} id="transactionType" className="top-padding">
+                                        <FloatingLabel controlId="floatingInputGrid" label="Transaction Type">
+                                            <Form.Control onChange={handleInputChange} name="Transaction-type" value={values["Transaction-type"]} type="text" 
+                                            placeholder="Transaction Type" ref={transactionType} required />
+                                        </FloatingLabel>
+                                    </Form.Group>
+                                </Row>
+                                <div className="top-padding w-100">
+                                    <Button type="submit">Submit</Button>
+                                </div> 
+                            </Form>
+
                     </div>
                 </div>
+
             </Container>
         </>
     );
